18ed6b01c40bf27560f220d0f645bac15a469eac
Mulia Arifandy Nasution <mul14.net@gmail.com>
2014-05-23 14:20:58 +0700

    Fixing the f*ck*ng tabs

--- app/database/migrations/2014_05_23_064656_create_users_table.php
+++ app/database/migrations/2014_05_23_064656_create_users_table.php
@@ -5,31 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table)
+        {
             $table->increments('id');
             $table->string('username');
             $table->string('password', 60);
             $table->string('remember_token', 100)->nullable();
-			$table->timestamps();
-		});
-	}
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
 
 }
+
